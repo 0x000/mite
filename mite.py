09cff09db2f1bb5f2fb34f3de1113a9c0f0b1939
Boban Spiroski <b.spiroski@outlook.com>
2015-07-26 09:34:31 +0200

    Refactoring

--- mite.py
+++ mite.py
@@ -11,30 +11,47 @@ def resolve(identifier, context):
         context = context[i]
     return context
 
+
 class Fragment:
     def __init__(self, raw):
         self.raw = raw
-        self.clean = self.clean_fragment()
-
-    def clean_fragment(self):
-        """ Returns the cleaned fragment string. """
-        clean = self.raw
-        if self.type == FRAG_VAR:
-            clean = clean.replace(TOKEN_OPEN, '').replace(TOKEN_CLOSE, '')
-            return clean.strip()
-        return clean
 
     def render(self, context):
         """ Returns the rendered string. """
-        if self.type == FRAG_VAR:
-            return str(resolve(self.clean, context))
-        return self.raw
+        return ''
 
     @property
     def type(self):
         if self.raw.startswith(TOKEN_OPEN):
             return FRAG_VAR
         return FRAG_TEXT
+
+
+class Text(Fragment):
+    def render(self, context):
+        return self.raw
+
+    @property
+    def type(self):
+        return FRAG_TEXT
+
+
+class Var(Fragment):
+    def __init__(self, raw):
+        self.raw = raw
+        self.identifier = self.clean_fragment()
+
+    def clean_fragment(self):
+        raw = self.raw
+        raw = raw.replace(TOKEN_OPEN, '').replace(TOKEN_CLOSE, '')
+        return raw.strip()
+
+    def render(self, context):
+        return str(resolve(self.identifier, context))
+
+    @property
+    def type(self):
+        return FRAG_VAR
 
 
 class Compiler:
@@ -44,8 +61,14 @@ class Compiler:
         self.output = self.compile()
 
     def compile(self):
-        """ Returns a list of Fragment objects. """
-        return [Fragment(frag) for frag in self.fragments]
+        """ Returns a list of Var or Text objects. """
+        output = []
+        for frag in self.fragments:
+            if Fragment(frag).type == FRAG_VAR:
+                output.append(Var(frag))
+            elif Fragment(frag).type == FRAG_TEXT:
+                output.append(Text(frag))
+        return output
 
     def _render_fragments(self, context):
         """ Yields rendered Fragments. """
